4bcac6ff5144e7ac76601aa45fd0949bbb514dfe
Kleyton Henrique dos Santos <kleytonhsantos@gmail.com>
2024-11-16 14:20:59 -0300

    :zap: perf: Create new test for performance

--- loadtest.js
+++ loadtest.js
@@ -6,27 +6,19 @@ export let errorRate = new Rate('errors');
 
 export let options = {
     thresholds: {
-        errors: ['rate<0.1'], // Limite de erros para o teste de carga
+        errors: ['rate<0.1'],
     },
     stages: [
-        { duration: '1m', target: 10 }, // Aumenta para 10 usuários em 1 minuto
-        { duration: '2m', target: 10 }, // Mantém 10 usuários por 2 minutos
-        { duration: '1m', target: 0 },  // Diminui para 0 usuários em 1 minuto
+        { duration: '1m', target: 10 },
+        { duration: '2m', target: 10 },
+        { duration: '1m', target: 0 },
     ],
 };
 
 export default function () {
-    let res = http.get('http://localhost:8000/health/liveness');
-    check(res, {
-        'status was 200': (r) => r.status === 200,
-    }) || errorRate.add(1);
+    let res;
 
-    res = http.get('http://localhost:8000/health/readiness');
-    check(res, {
-        'status was 200': (r) => r.status === 200,
-    }) || errorRate.add(1);
-
-    res = http.get('http://localhost:8000/products/all');
+    res = http.get('http://localhost:8000/products/python');
     check(res, {
         'status was 200': (r) => r.status === 200,
     }) || errorRate.add(1);
@@ -34,6 +26,13 @@ export default function () {
     res = http.get('http://localhost:8000/products/python/3.11');
     check(res, {
         'status was 200': (r) => r.status === 200,
+        'eol exists': (r) => r.json().eol !== undefined,
+    }) || errorRate.add(1);
+
+    res = http.get('http://localhost:8000/products/python/3.11/summarized');
+    check(res, {
+        'status was 200': (r) => r.status === 200,
+        'eol exists': (r) => r.json().eol !== undefined,
     }) || errorRate.add(1);
 
     sleep(1);
